b6254c94bb671ce1e1e342ede10d57b6f34a0f3b
Guillaume Millot <guillaume.millot@gmail.com>
2020-06-26 16:53:24 +0200

    Update simple_test.c

--- simple_test.c
+++ simple_test.c
@@ -4,7 +4,12 @@ int main(void) {
     char buf[7];
     int in;
     in = scanf("%6s", buf);
-    printf("in = %d",in);
-    printf("buf = %6s",buf);
+    if (in == EOF) {
+        printf("Read nothing!");
+    } 
+    else {
+        printf("Read %d characters.", in);
+        printf("buf = %6s", buf);
+    }
     return 0;
 }
